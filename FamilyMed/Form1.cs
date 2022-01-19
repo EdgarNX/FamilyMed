5d3f6c50e07a264e1b803e7c87432c21cf46b101
EdgarNX <edgarnemeth@gmail.com>
2022-01-19 22:44:01 +0200

    database connection ready

---
+++ FamilyMed/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+namespace FamilyMed
+{
+    public partial class Form1 : Form
+    {
+
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "uqAijVuxmGKZXy2G79A9eADyjInVZ1NgEDQLmI1p",
+            BasePath = "https://familymed-297e1-default-rtdb.europe-west1.firebasedatabase.app/"
+        };
+
+        IFirebaseClient client;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new FireSharp.FirebaseClient(config);
+
+            if(client!=null)
+            {
+                MessageBox.Show("Connection is established!");
+            }
+        }
+    }
+}
